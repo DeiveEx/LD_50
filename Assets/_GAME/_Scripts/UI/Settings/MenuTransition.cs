8af626458e658886fc46acf5831307502c1883e0
Vitor Ribeiro <vitordex@hotmail.com>
2022-04-04 20:21:29 -0300

    feat(game): Add settings menu transition

---
+++ Assets/_GAME/_Scripts/UI/Settings/MenuTransition.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace _GAME._Scripts.UI.Settings
+{
+    public class MenuTransition : MonoBehaviour
+    {
+        public RectTransform container;
+        public float speed = 4f;
+
+        public RectTransform target;
+        public RectTransform openedReference;
+        public RectTransform closedReference;
+
+        public void Open() => SetTarget(openedReference);
+        public void Close() => SetTarget(closedReference);
+
+        public void SetTarget(RectTransform rectTransform) => 
+            target = rectTransform;
+
+        private void Update()
+        {
+            container.position = Vector3.Lerp(
+                container.position, 
+                target.position, 
+                Time.deltaTime * speed);
+        }
+    }
+}
